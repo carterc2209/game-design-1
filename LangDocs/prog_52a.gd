0612d938081a20ecd7eee181ade83a77996432af
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2024-09-05 14:24:08 -0500

    Update 9/5

---
+++ LangDocs/prog_52a.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+
+func _on_calculate_pressed() -> void:
+	var length = int($Lengthtxt.text)
+	var width = int($Widthtxt.text)
+	var area = length * width
+	var perimeter = 2 * length + 2 * width
+	$Area.text = "Area: " + str(area)
+	$Perimeter.text = "Perimeter: " + str(perimeter)
+	# Operators: + - * / %   **pow
+	# str - string (text)
+	# int - integer (whole number)
+	# float - floating-point number (w/ decimal)
+
+func _on_clear_pressed() -> void:
+	$LengthTxt.text = ""
+	$WidthTxt.text = ""
+	$Area.text = ""
+	$Perimeter.text = ""
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
