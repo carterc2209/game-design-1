06fea4243a2d0efbc3bd702f5b288d3c477bcb21
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2024-09-09 15:28:00 -0500

    prog_93 9/9

--- LangDocs/prog_93a.gd
+++ LangDocs/prog_93a.gd
@@ -4,13 +4,17 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_calc_pressed() -> void:
-	var rate = float($Watts.text)
+	var w = ($Watts.text)
+	var d = float(0.0475)
+	var rate = float(d) * float(w)
 	var sur = float(rate * .1)
 	var cty = float(rate * .03)
 	var ttl = rate + sur + cty
-	($City.text) = "City Tax:" + cty
-	($Surcharge.text) = "Surcharge:" + sur
-	($Total.text) = "Total:" + ttl
+	var late = ttl + (ttl * 1.04)
+	($Total.text) = "Total: %.3f" % ttl
+	($City.text) = "City Tax: %.3f" % cty
+	($Surcharge.text) = "Surcharge:" + str(sur)
+	($Late.text) = str(late)
 
 func _on_clear_pressed() -> void:
 	($City.text) = "City Tax:"
