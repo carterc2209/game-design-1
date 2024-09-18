e716f3277cf7337a56f716891572e79b728ab62e
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2024-09-18 14:11:57 -0500

    Prog115A 9/18

---
+++ prog_115a.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+
+
+func _on_calc_pressed() -> void:
+	$ItemList.add_item("Number")
+	# range(stop)
+	# range(start, stop)
+	# ramge(start, stop, step)
+	# the 'stop' number is excluded, so add 1 if needed.
+	for num in range (2, 36 + 1, 2):
+		var line = str(num)
+		$ItemList.add_item(line)
+
+
+func _on_clear_pressed() -> void:
+	$ItemList.clear()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
