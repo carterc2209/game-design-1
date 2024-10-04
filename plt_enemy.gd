a0a7a6122161fa4f6c240ce3bd1a35a00e5cf205
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2024-10-04 11:54:56 -0500

    World Edits 10/4

---
+++ plt_enemy.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+var speed = 100
+var direction = -1
+
+
+func _ready() -> void:
+	$Timer.start()
+
+
+func _physics_process(_delta: float) -> void:
+	self.linear_velocity.x = speed * direction
+
+
+
+func _on_timer_timeout() -> void:
+	direction *= -1
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.name == "plt_player":
+		body.queue_free()
+		OS.alert("You Died!")
+		get_tree().reload_current_scene()
