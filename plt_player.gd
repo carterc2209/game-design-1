eaf3f288d126bd5019b73498c07174c901e53121
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2024-10-10 15:24:57 -0500

    Pause Menu & World updates 10/10

--- plt_player.gd
+++ plt_player.gd
@@ -48,6 +48,9 @@ func _physics_process(delta: float) -> void:
 		$Camera2D.zoom += Vector2(1, 1)
 	if Input.is_action_just_pressed("zoom_cam_out"):
 		$Camera2D.zoom -= Vector2(1, 1)
+	if Input.is_action_just_pressed("Pause"):
+		$Camera2D/PauseMenu.show()
+		get_tree().paused = true
 
 	move_and_slide()
 
