858f0f0929e1ca5bac6cb75ae47fd495be6e0235
carterc2209 <campbell.c@stu.janesville.k12.wi.us>
2024-11-01 14:42:36 -0500

    Player HUD

---
+++ player_hud.gd
@@ -0,0 +1,52 @@
+extends CanvasLayer
+
+@onready var player = get_tree().get_first_node_in_group("Player")
+@onready var hearts = $PlayerHealth/Hearts
+
+const HEART_ROW_SIZE = 10
+const HEART_OFFSET = 16
+
+func create_heart():
+	var n_heart = Sprite2D.new()
+	n_heart.texture = hearts.texture
+	n_heart.hframes = hearts.hframes
+	n_heart.vframes = hearts.vframes
+	n_heart.frame = 8
+	hearts.add_child(n_heart)
+	
+
+@onready var p_HUD = get_tree().get_first_node_in_group("HUD")
+
+func _ready() -> void:
+	draw_hearts()
+
+
+func draw_hearts():
+	for heart in hearts.get_children():
+		hearts.remove_child(hearts)
+	for i in range(int(player.data.max_health) / 20):
+		create_heart() # 1 heart per 20 hp
+
+
+
+
+
+func _process(delta: float) -> void:
+	var p_health = player.data.health
+	var full_hearts = floor(p_health / 20)
+	var remainder = int(p_health) % 20
+	
+	for heart in hearts.get_children():
+		var index = heart.get_index()
+		var x = (index % HEART_ROW_SIZE) * HEART_OFFSET 
+		var y = (index / HEART_ROW_SIZE) * HEART_OFFSET
+		heart.position = Vector2(x, y)
+		
+		# frame 8 empty. frame 7 1/4. frame 6 1/2. frame 5 3/4. frame 4 full
+		if index > full_hearts:
+			hearts.frame = 8
+		elif index == full_hearts:
+			heart.frame = 8 - int(remainder / 5)
+		elif index < full_hearts:
+			heart.frame = 4
+	pass
--- td_player.gd
+++ td_player.gd
@@ -2,8 +2,31 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
+const MAXIMUM_OBTAINABLE_HEALTH = 400.0
+enum  STATES { IDLE=0, DEAD, DAMAGED, ATTACKING, CHARGING }
+
+@export var data = {
+	"max_health": 60.0,  # 20hp per heart, 5 per fraction
+	"health": 60.0,      # Min 60 Max 400
+	"money": 0,
+	"state": STATES.IDLE,
+	"secondaries": [],
+}
+
 var inertia = Vector2()
-var LookDirection = Vector2.DOWN # Vector2(0, 1)
+var look_direction = Vector2.DOWN  # Vector2(0,1)
+
+@onready var p_HUD = get_tree().get_first_node_in_group("HUD")
+
+func _ready() -> void:
+	p_HUD.show()
+
+func pickup_health(value):
+	data.health += value
+	data.health = clamp(data.health, 0, data.max_health)
+
+func pickup_money(value):
+	data.money += value
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector2(
@@ -11,16 +34,33 @@ func _physics_process(delta: float) -> void:
 		Input.get_axis("ui_up", "ui_down")
 	)
 	if direction.length() > 0:
-		LookDirection = direction
+		look_direction = direction
 		# Scale to 1 to prevent speed boost from diagonals
 		direction = direction.normalized()
-		velocity = direction *  SPEED
+		velocity  = direction * SPEED
 	else:
-		velocity = velocity.move_toward(Vector2. ZERO, SPEED)
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	velocity += inertia
+	update_animation(direction)
 	move_and_slide()
-	inertia = inertia.move_toward(Vector2.ZERO, delta * 1000.00)
+	inertia = inertia.move_toward(Vector2.ZERO, delta * 1000.0)
 	
 	if Input.is_action_just_pressed("ui_cancel"):
-		$Camera2D/PauseMenu.show()
+		$Camera2D/pause_menu.show()
 		get_tree().paused = true
+
+
+func update_animation(direction):
+	var a_name = "Idle_"
+	if direction.length() > 0:
+		a_name = "Walk_"
+	if look_direction.x != 0:
+		a_name += "Side"
+		$AnimatedSprite2D.flip_h = look_direction.x < 0
+	elif look_direction.y < 0:
+		a_name += "Up"
+	elif look_direction.y > 0:
+		a_name += "Down"
+	$AnimatedSprite2D.animation = a_name
+	$AnimatedSprite2D.play()
+	pass
